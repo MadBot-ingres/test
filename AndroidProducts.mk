81c7dbde01bba1c62913a07e974cca3f59eaba5f
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2024-10-10 14:49:15 +0330

    ingres: Import lineage_ingres instead of lineage_zeus and inherit COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_zeus.mk
+    $(LOCAL_DIR)/lineage_ingres.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_ingres-user \
+    lineage_ingres-userdebug \
+    lineage_ingres-eng
