ddf95cd63d1b8000503fb32cf1eb2ea006e89299
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2024-10-10 14:53:36 +0330

    ingres: Inherit some shits for screen and kernel (we use prebuilt for now)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,19 +8,26 @@
 include device/xiaomi/sm8450-common/BoardConfigCommon.mk
 
 # Inherit from the proprietary version
-include vendor/xiaomi/zeus/BoardConfigVendor.mk
+include vendor/xiaomi/ingres/BoardConfigVendor.mk
 
-DEVICE_PATH := device/xiaomi/zeus
+DEVICE_PATH := device/xiaomi/ingres
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := ingres
+TARGET_NO_BOOTLOADER := true
 
 # Kernel
-device_second_stage_modules := \
-    goodix_fod.ko \
-    qcom_pm8008-regulator.ko \
-    fts_touch_spi.ko \
-    aw8697-haptic.ko
+BOARD_BOOTIMG_HEADER_VERSION := 4
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_KERNEL_IMAGE_NAME := Image
+TARGET_KERNEL_CONFIG := ingres_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/ingres
 
-device_vendor_dlkm_exclusive_modules := \
-    cs35l41_dlkm.ko
+# Kernel - prebuilt
+TARGET_FORCE_PREBUILT_KERNEL := true
+ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+endif
 
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(device_second_stage_modules)
 BOARD_VENDOR_KERNEL_MODULES_LOAD += $(device_second_stage_modules) $(device_vendor_dlkm_exclusive_modules)
@@ -32,4 +39,4 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # Screen density
-TARGET_SCREEN_DENSITY := 560
+TARGET_SCREEN_DENSITY := 395
