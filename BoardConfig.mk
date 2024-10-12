978b012d6c32e4f84c3127c0b38aa0f2626f1a8b
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2024-10-12 17:16:46 +0330

    ingres: Update the BoardConfig makefile with two prebuilt images
    [TEMP] This is temp do not worry OSS Vendor and Kernel comes out soon.
    This commit adds prebuilt kernel and vendor_dlkm

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,23 +16,25 @@ DEVICE_PATH := device/xiaomi/ingres
 TARGET_BOOTLOADER_BOARD_NAME := ingres
 TARGET_NO_BOOTLOADER := true
 
+# Use prebuilt kernel
+TARGET_PREBUILT_KERNEL := device/xiaomi/ingres/prebuilt/Image
+
 # Kernel
+TARGET_FORCE_PREBUILT_KERNEL := true
+TARGET_NO_KERNEL_OVERRIDE := true
 BOARD_BOOTIMG_HEADER_VERSION := 4
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_KERNEL_IMAGE_NAME := Image
-TARGET_KERNEL_CONFIG := ingres_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/ingres
+PRODUCT_COPY_FILES += $(TARGET_PREBUILT_KERNEL):kernel
 
-# Kernel - prebuilt
-TARGET_FORCE_PREBUILT_KERNEL := true
-ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
-endif
+# Disable kernel build from source
+TARGET_KERNEL_CONFIG := 
+TARGET_KERNEL_SOURCE :=
+INLINE_KERNEL_BUILDING := true
 
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(device_second_stage_modules)
-BOARD_VENDOR_KERNEL_MODULES_LOAD += $(device_second_stage_modules) $(device_vendor_dlkm_exclusive_modules)
-
-BOOT_KERNEL_MODULES += $(device_second_stage_modules)
+# Vendor_dlkm - prebuilt
+TARGET_PREBUILT_VENDOR_DLKM := device/xiaomi/ingres/prebuilt/vendor_dlkm.img
+PRODUCT_COPY_FILES += $(TARGET_PREBUILT_VENDOR_DLKM):vendor_dlkm.img
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
